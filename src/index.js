6afb53319761b89a8efac2bd5ac8e3db6a80a829
Mona Salari <monaslr95@yahoo.com>
2026-01-02 14:17:50 -0600

    adding some JS and api to the sie

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 function generatePoem(event) {
   event.preventDefault();
+
+  let promptInput = document.querySelector("#user-input");
+
+  let apiKey ="3374e73o90b5b05b6faa166tf86b3d63";
+  let context ="You are a Persian poet. Write beautiful and meaningful Persian poems.";
+  let prompt =`Write a Persian poem about ${promptInput.value} in 4 lines.seprate the lines with <br>.also add Mona's AI Generator signature at the end of the poem with <strong> element.`;
+  let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: " بر چهره گل نسیم نوروز خوش است",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
